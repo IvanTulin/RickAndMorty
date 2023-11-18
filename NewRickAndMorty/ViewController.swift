196e23378a6e67cfceafebbfb793a3da0bfd2992
Ivan Tulin <tulin.v@bk.ru>
2023-11-18 14:04:29 +0300

    Initial Commit

---
+++ NewRickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewRickAndMorty
+//
+//  Created by Ivan Tulin on 18.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
